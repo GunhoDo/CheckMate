bd8a63e500b53d519db1afb111e094364b65aeca
GunhoDo <eh5890@gmail.com>
2025-05-28 11:01:36 +0900

    import

--- src/main/java/goldstamp/two/service/MemberService.java
+++ src/main/java/goldstamp/two/service/MemberService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.lang.IllegalArgumentException;
 import java.util.List;
-import java.util.Optional; // findById 사용을 위해 Optional import
 
 @Service
 @Transactional(readOnly = true)

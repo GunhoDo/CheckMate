efbeace6c4d24a5d59e07176c09d3746dd8abdce
GunhoDo <eh5890@gmail.com>
2025-05-31 20:29:46 +0900

    error

--- src/main/java/goldstamp/two/dto/DiseaseDto.java
+++ src/main/java/goldstamp/two/dto/DiseaseDto.java
@@ -1,8 +1,4 @@
 package goldstamp.two.dto;
-<<<<<<< HEAD
-
-=======
->>>>>>> e94cf969e26ee792cce3f776e11456f3de18ff1f
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +12,4 @@ public class DiseaseDto {
     private Long id;
     private String name;
     private String explain;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e94cf969e26ee792cce3f776e11456f3de18ff1f
